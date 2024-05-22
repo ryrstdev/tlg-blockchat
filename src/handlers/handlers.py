f6bc504c8ff6d080e525936a36bb8ac3b0b2a213
ryrstdev <rycbizall@gmail.com>
2024-05-22 13:46:56 +0800

    Update handlers.py

--- src/handlers/handlers.py
+++ src/handlers/handlers.py
@@ -16,39 +16,6 @@ async def security_check(event: NewMessage) -> None:
     client = event.client
     chat_id = event.chat_id
     await client(SetTypingRequest(peer=chat_id, action=SendMessageTypingAction()))
-    if chat_id not in ALLOW_USERS:
-        await client.send_message(
-            chat_id, f"This is personal property, you are not allowed to proceed!"
-        )
-        raise StopPropagation
-
-
-@register(NewMessage(pattern="/search"))
-async def search_handler(event: NewMessage) -> None:
-    client = event.client
-    chat_id = event.chat_id
-    await client(SetTypingRequest(peer=chat_id, action=SendMessageTypingAction()))
-    response = await search(event)
-    try:
-        await client.send_message(chat_id, f"__Here is your search:__\n{response}")
-        logging.info(f"Sent /search to {chat_id}")
-    except Exception as e:
-        logging.error(f"Error occurred: {e}")
-        await event.reply("**Fail to get response**")
-    await client.action(chat_id, "cancel")
-    raise StopPropagation
-
-
-@register(NewMessage(pattern="/bash"))
-async def bash_handler(event: NewMessage) -> None:
-    client = event.client
-    response = await bash(event)
-    try:
-        await client.send_message(event.chat_id, f"{response}")
-        logging.info(f"Sent /bash to {event.chat_id}")
-    except Exception as e:
-        logging.error(f"Error occurred while responding /bash cmd: {e}")
-    raise StopPropagation
 
 
 @register(NewMessage(pattern="/clear"))
